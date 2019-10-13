2cfa959a52f1c3e431dc542ea96914555eeb42cc
fya0122 <18730930122@163.com>
2019-10-13 11:01:05 +0800

    'homeBook写完了'

--- src/components/home/HomeBook.vue
+++ src/components/home/HomeBook.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="home-book">
     <div class="home-book-header">{{ title }}</div>
-    <div class="home-book-content">2</div>
-    <div class="home-book-footer">3</div>
+    <div class="home-book-content">
+      <!-- 再次重申：实际开发，不要用index当做key -->
+      <div class="home-book-row" v-for="(item, index) of bookData" :key="index">
+        <div :style="{ flex: '0 0 ' + (100 / col) + '%' }" class="home-book-col" v-for="(book, bookIndex) of item" :key="bookIndex">
+          <div @click="onBookClick" :style="{flexDirection: mode === HOME_BOOK_MODE.col ? 'col' : 'row'}" class="book-wrapper">
+            <image-view :src="book.cover"></image-view>
+            <div v-if="mode === HOME_BOOK_MODE.col" class="book-title-wrapper book-title-col">
+              <div class="book-title">{{ book.title }}</div>
+            </div>
+            <div v-else class="book-title-wrapper book-title-row">
+              <div class="book-title">{{ book.title }}</div>
+              <div class="book-title-author-wrapper">
+                <div class="book-title book-author">{{ book.author }}</div>
+                <div class="book-title book-author">{{ book.categoryText }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="home-book-footer" v-if="showBtn" @click="onMoreClick">
+      <van-button round custom-class="home-book-btn">{{ btnText }}</van-button>
+    </div>
   </div>
 </template>
 <script>
 import { HOME_BOOK_MODE } from '../../utils/const'
+import ImageView from '../base/ImageView'
 
 export default {
+  components: {
+    ImageView
+  },
   props: {
     // 标题栏
     title: {
@@ -59,9 +84,38 @@ export default {
   },
   methods: {
     // 点击更多按钮事件
-    onMoreClick () {},
+    onMoreClick () {
+      this.$emit('onMoreClick')
+    },
     // 图书点击事件
-    onBookClick () {}
+    onBookClick () {
+      this.$emit('onBookClick')
+    }
+  },
+  mounted () {
+    console.log(this.bookData)
+  },
+  computed: {
+    HOME_BOOK_MODE () {
+      return HOME_BOOK_MODE
+    },
+    /* 二维数组 */
+    bookData () {
+      const { col, row, data } = this
+      if (data && data.length > 0) {
+        const number = row * col
+        const _bookData = data.slice(0, number)
+        const _bookDataRow = []
+        let _row = 0
+        while (_row < row) {
+          _bookDataRow.push(_bookData.slice(_row * col, _row * col + col))
+          _row++
+        }
+        return _bookDataRow
+      } else {
+        return []
+      }
+    }
   }
 }
 </script>
@@ -70,7 +124,65 @@ export default {
   .home-book-header {
     padding: 13px 0 0 20.5px;
   }
-  .home-book-content {}
-  .home-book-footer {}
+  .home-book-content {
+    padding: 0 12px;
+    margin-top: 10.5px;
+    .home-book-row {
+      display: flex;
+      flex-flow: row nowrap;
+      margin-top: 12px;
+      .home-book-col {
+        padding: 0 8px;
+        box-sizing: border-box;
+        .book-wrapper {
+          display: flex;
+          .book-title-wrapper {
+            &.book-title-col {
+              .book-title {
+                font-size: 12px;
+                color: #212731;
+                line-height: 16.5px;
+                max-height: 33px;
+                font-weight: 500;
+                overflow: hidden;
+                word-break: break-word;
+              }
+            }
+            &.book-title-row {
+              flex: 0 0 50%;
+              padding: 10px;
+              display: flex;
+              flex-direction: column;
+              justify-content: space-between;
+              .book-title {
+                font-size: 14px;
+                color: #1f1f1f;
+                line-height: 18px;
+                max-height: 36px;
+                overflow: hidden;
+                word-break: break-word;
+              }
+              .book-author {
+                font-size: 12px;
+                color: #868686;
+                line-height: 14px;
+                max-height: 14px;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .home-book-footer {
+    margin-top: 12px;
+    padding: 12px 20px 20px;
+    >>>.home-book-btn {
+      width: 100%;
+      font-size: 14px;
+      color: #3696ef;
+      border: 1px solid #edeeee;
+    }
+  }
 }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="home">
     <search-bar @onClick="onSearchBarClick" :disabled="true"></search-bar>
-<!--    <image-view height="200px" mode="scaleToFill" src="https://www.youbaobao.xyz/mpvue-res/big.jpg"></image-view>-->
+    <!--    <image-view height="200px" mode="scaleToFill" src="https://www.youbaobao.xyz/mpvue-res/big.jpg"></image-view>-->
     <home-card></home-card>
     <home-banner @onClick="onBannerClick" img="http://www.youbaobao.xyz/book/res/bg.jpg" title="mpvue2.0实战多端小程序上线啦" sub-title="立即体验"></home-banner>
     <div :style="{marginTop: '23px'}">
-      <home-book title="为你推荐"></home-book>
+      <home-book @onMoreClick="onMoreClick" :btn-text="'换一批'" :mode="'col'" :data="data" :row="2" :col="1" title="为你推荐"></home-book>
     </div>
   </div>
 </template>
-
 
 
 <script>
@@ -18,6 +17,7 @@ import SearchBar from '../../components/home/SearchBar'
 import HomeCard from '../../components/home/HomeCard'
 import HomeBanner from '../../components/home/HomeBanner'
 import HomeBook from '../../components/home/HomeBook'
+
 export default {
   components: {
     HomeBook,
@@ -26,12 +26,73 @@ export default {
     HomeCard,
     HomeBanner
   },
+  data () {
+    return {
+      data: [
+        {
+          'id': 34,
+          'fileName': '2018_Book_GeographiesOfTheUniversity',
+          'cover': 'https://www.youbaobao.xyz/book/res/img/Geography/978-3-319-75593-9_CoverFigure.jpg',
+          'title': 'Geographies of the University',
+          'author': 'Peter Meusburger',
+          'publisher': 'Springer International Publishing',
+          'bookId': '2018_Book_GeographiesOfTheUniversity',
+          'category': 7,
+          'categoryText': 'Geography',
+          'language': 'en',
+          'rootFile': 'OEBPS/package.opf'
+        },
+        {
+          'id': 59,
+          'fileName': '2018_Book_DrinkingInVictorianAndEdwardia',
+          'cover': 'https://www.youbaobao.xyz/book/res/img/History/978-3-319-92964-4_CoverFigure.jpg',
+          'title': 'Drinking in Victorian and Edwardian Britain',
+          'author': 'Thora Hands',
+          'publisher': 'Springer International Publishing',
+          'bookId': '2018_Book_DrinkingInVictorianAndEdwardia',
+          'category': 8,
+          'categoryText': 'History',
+          'language': 'en',
+          'rootFile': 'OEBPS/package.opf'
+        },
+        {
+          'id': 60,
+          'fileName': '2018_Book_HarnessingThePowerOfTheCrimina',
+          'cover': 'https://www.youbaobao.xyz/book/res/img/History/978-3-319-77908-9_CoverFigure.jpg',
+          'title': 'Harnessing the Power of the Criminal Corpse',
+          'author': 'Sarah Tarlow',
+          'publisher': 'Springer International Publishing',
+          'bookId': '2018_Book_HarnessingThePowerOfTheCrimina',
+          'category': 8,
+          'categoryText': 'History',
+          'language': 'en',
+          'rootFile': 'OEBPS/package.opf'
+        },
+        {
+          'id': 75,
+          'fileName': '2018_Book_TheEuropeanBloodAndMarrowTrans',
+          'cover': 'https://www.youbaobao.xyz/book/res/img/MedicineAndPublicHealth/978-3-319-50026-3_CoverFigure.jpg',
+          'title': 'The European Blood and Marrow Transplantation Textbook for Nurses',
+          'author': 'Michelle Kenyon',
+          'publisher': 'Springer International Publishing',
+          'bookId': '2018_Book_TheEuropeanBloodAndMarrowTrans',
+          'category': 17,
+          'categoryText': 'MedicineAndPublicHealth',
+          'language': 'en',
+          'rootFile': 'OEBPS/package.opf'
+        }
+      ]
+    }
+  },
   methods: {
     onSearchBarClick () {
       // 跳转到搜索页面
     },
     onBannerClick () {
       console.log(123)
+    },
+    onMoreClick () {
+      console.log('点击了更多')
     }
   }
 }
